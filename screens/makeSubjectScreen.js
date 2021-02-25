e4bea3712227ff334fe2987572fe7b36c4f65997
poom-cyber <poompim083@gmail.com>
2021-02-25 21:11:50 +0700

    first

--- screens/makeSubjectScreen.js
+++ screens/makeSubjectScreen.js
@@ -1,73 +1,92 @@
 import * as React from 'react';
-import {useContext, Component} from 'react';
-import {View, StyleSheet, Text, Alert} from 'react-native';
+import { useContext, Component} from 'react'
+import {View, StyleSheet, Text, Alert } from 'react-native';
 import {FilledButton} from '../components/FilledButton';
-import {AuthContext} from '../navigaiton/AuthProvider';
+import { AuthContext } from '../navigaiton/AuthProvider';
 import firestore from '@react-native-firebase/firestore';
-import {Input, ListItem, Button} from 'react-native-elements';
-import {ScrollView} from 'react-native-gesture-handler';
+import { Input, ListItem, Button } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const subject = 'sssss';
+const subject = 'sssss'
 
 const editSubject = () => {
-  // subject = this.subject
-  Alert.alert(subject);
-};
-
-class addSubject extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Input
-            placeholder="Question"
-            leftIcon={{type: 'font-awesome', name: 'book'}}
-            style={styles}
-            // value={this.subject}
-            // onChangeText={(val) => editSubject}
-          />
-
-          <FilledButton
-            title="ADD SUBJECT"
-            style={styles.loginButton}
-            onPress={() => this.props.navigation.navigate('MakeQuestion')}
-            // onPress={editSubject}
-          />
-          <FilledButton
-            title={'Logout'}
-            style={styles.loginButton}
-            onPress={() => logout()}
-          />
-        </View>
-      </ScrollView>
-    );
-  }
+    // subject = this.subject
+    Alert.alert(subject)
 }
 
+class addSubject extends Component {
+
+    constructor() {
+        super();
+
+    }
+    
+    render (){
+      return (
+        <ScrollView>
+          <View style={styles.container}>
+            
+            <FilledButton 
+                title='SUBJECT 1'
+                style={styles.loginButton} 
+                onPress={() => this.props.navigation.navigate('MakeQuestion')}
+                // onPress={editSubject}
+            />
+            <FilledButton 
+                title='SUBJECT 2'
+                style={styles.loginButton} 
+                onPress={() => this.props.navigation.navigate('MakeQuestion')}
+                // onPress={editSubject}
+            />
+            <FilledButton 
+                title='SUBJECT 3'
+                style={styles.loginButton} 
+                onPress={() => this.props.navigation.navigate('MakeQuestion')}
+                // onPress={editSubject}
+            />
+            <FilledButton 
+                title='SUBJECT 4'
+                style={styles.loginButton} 
+                onPress={() => this.props.navigation.navigate('MakeQuestion')}
+                // onPress={editSubject}
+            />
+            <FilledButton 
+                title='SUBJECT 5'
+                style={styles.loginButton} 
+                onPress={() => this.props.navigation.navigate('MakeQuestion')}
+                // onPress={editSubject}
+            />
+            
+            <FilledButton title={'Logout'} style={styles.loginButton} onPress={()=> logout()} />      
+          </View>
+        </ScrollView>
+      )
+    }
+
+}
+
+
 const styles = StyleSheet.create({
-  title: {
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  input: {
-    marginVertical: 10,
-    marginBottom: 15,
-  },
-  loginButton: {
-    marginVertical: 32,
-  },
+    title: {
+      marginBottom: 20,
+      textAlign: 'center',
+    },
+    input: {
+      marginVertical: 10,
+      marginBottom: 15,
+    },
+    loginButton: {
+      marginVertical: 32,
+    },
+  
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+      marginBottom: 100
+  
+    }
+  });
 
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-    marginBottom: 100,
-  },
-});
-
-export default addSubject;
+  export default addSubject;
